96476c5ac4f3f67d5e6aa9f55fe7acaad77cc86c
Beatrice-star <beatrice.pauletto010@gmail.com>
2025-06-23 11:30:02 +0200

    correcting js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function init(){
 	function handleClick(){
-	  let inputText = document.getElementById(entryinput).value;
-	  alert ('Beatrice Pauletto:' + entryinput);
+	  let inputText = document.getElementById('entryinput').value;
+	  alert ('Beatrice Pauletto:' + inputText);
 	  document.getElementById('textoutput').innerText = inputText;
-} }
+} 
+  document.getElementById('entryinput').addEventListener('click', handleClick);
+}
 //add your javascrip between these two lines of code
 window.addEventListener('load', init);
