1ceec3c404b7981eecc3cdf55d8b6eae68baa4a9
Beatrice-star <beatrice.pauletto010@gmail.com>
2025-06-23 11:23:15 +0200

    closing js

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ function init(){
 	  let inputText = document.getElementById(entryinput).value;
 	  alert ('Beatrice Pauletto:' + entryinput);
 	  document.getElementById('textoutput').innerText = inputText;
+} }
 //add your javascrip between these two lines of code
 window.addEventListener('load', init);
